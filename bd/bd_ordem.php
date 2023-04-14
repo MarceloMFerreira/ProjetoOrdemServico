44e628308060190d93e77782027a9fed9398f8ee
MarceloMFerreira <marcelomf.007@hotmail.com>
2023-04-14 11:18:42 -0300

    Up implementando Ordens de serviço

--- bd/bd_ordem.php
+++ bd/bd_ordem.php
@@ -57,7 +57,44 @@ function listaOrdem()
     return $ordem;
 }
 
-function  cadastraOrdem($cod_cliente,$cod_servico,$cod_terceirizado,$data_servico,$status,$data){
+function listaOrdemStatus($status, $codigo, $perfil)
+{
+    $conexao = conecta_bd();
+
+    $ordem = array();
+
+    $query = "SELECT o.cod AS cod,
+           c.nome AS nome_cliente,
+           t.nome AS nome_terceirizada,
+           s.nome AS nome_servico,
+           o.data_servico AS data_servico,
+           o.status AS status
+           FROM ordem o
+           INNER JOIN servico s ON o.cod_servico = s.cod
+           INNER JOIN cliente c ON o.cod_cliente = c.cod
+           INNER JOIN terceirizado t ON o.cod_terceirizado = t.cod
+           WHERE o.status = '$status'";
+
+    if ($perfil == 2)
+        $query .= " AND o.cod_cliente = '$codigo'";
+    else if ($perfil == 3)
+        $query .= " AND o.cod_terceirizado = '$codigo'";
+
+
+    $query .= " ORDER BY o.status ASC";
+
+
+    $resultado = mysqli_query($conexao, $query);
+
+    while ($dados = mysqli_fetch_array($resultado)) {
+        array_push($ordem, $dados);
+    }
+    return $ordem;
+}
+
+
+function  cadastraOrdem($cod_cliente, $cod_servico, $cod_terceirizado, $data_servico, $status, $data)
+{
     $conexao = conecta_bd();
 
     $query = "insert into ordem(cod_cliente,cod_servico,cod_terceirizado,data_servico,status,data) values('$cod_cliente','$cod_servico','$cod_terceirizado','$data_servico','$status','$data')";
@@ -67,7 +104,8 @@ function  cadastraOrdem($cod_cliente,$cod_servico,$cod_terceirizado,$data_servic
     return $dados;
 }
 
-function buscaOrdemadd(){
+function buscaOrdemadd()
+{
     $conexao = conecta_bd();
 
     $query = "Select
@@ -80,14 +118,15 @@ function buscaOrdemadd(){
 
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_array($resultado);
-  
-    
+
+
     return $dados;
 }
 
-function buscaOrdemeditar($codigo){
+function buscaOrdemeditar($codigo)
+{
     $conexao = conecta_bd();
-	$query = "Select 
+    $query = "Select 
 			  o.cod AS cod,
 			  c.nome AS nome_cliente,
 			  t.nome AS nome_terceirizada,
@@ -100,30 +139,47 @@ function buscaOrdemeditar($codigo){
 			        o.cod_servico = s.cod AND 
 			        o.cod_terceirizado = t.cod AND
 			        o.cod = '$codigo'";
-						  
-	$resultado = mysqli_query($conexao, $query);
-	$dados = mysqli_fetch_array($resultado);
 
-	return $dados;
+    $resultado = mysqli_query($conexao, $query);
+    $dados = mysqli_fetch_array($resultado);
+
+    return $dados;
 }
 
-function editarOrdem($codigo,$cod_terceirizado,$data_servico,$status,$data){
+function editarOrdem($codigo, $cod_terceirizado, $data_servico, $status, $data)
+{
     $conexao = conecta_bd();
     $query = "SELECT * 
               FROM ordem
               WHERE cod='$codigo'";
-  
-    $resultado = mysqli_query($conexao,$query);
+
+    $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_num_rows($resultado);
-  
-    if($dados == 1){
-      $query = "UPDATE  ordem
+
+    if ($dados == 1) {
+        $query = "UPDATE  ordem
                 SET cod_terceirizado = '$cod_terceirizado', data_servico = '$data_servico', status = '$status', data = '$data'
                 WHERE cod = '$codigo'";
-       $resultado = mysqli_query($conexao,$query);
-       $dados = mysqli_affected_rows($conexao);
-       return $dados;
+        $resultado = mysqli_query($conexao, $query);
+        $dados = mysqli_affected_rows($conexao);
+        return $dados;
     }
-  }
+}
 
-?>
+function removeOrdem($codigo)
+{
+    $conexao = conecta_bd();
+    $query = "SELECT * 
+              FROM ordem
+              WHERE cod='$codigo'";
+
+    $resultado = mysqli_query($conexao, $query);
+    $dados = mysqli_num_rows($resultado);
+
+    if ($dados == 1) {
+        $query = "DELETE FROM ordem WHERE cod='$codigo'";
+        $resultado = mysqli_query($conexao, $query);
+        $dados = mysqli_affected_rows($conexao);
+        return $dados;
+    }
+}
